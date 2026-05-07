4b7f376d2263c27cb36a4c37d427a4a4eec3ab56
Fere <tiran.sc.f@gmail.com>
2026-05-07 09:55:56 -0600

    agregar: OpenAPI/Swagger para documentacion de la API

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,24 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import { readFileSync } from "fs";
+import { join } from "path";
 import categoryRoutes from "./routes/category.routes";
 import videoRoutes from "./routes/video.routes";
 import { errorHandler, notFoundHandler } from "./middlewares/error.middleware";
 
 const app = express();
 
+const swaggerDocument = JSON.parse(readFileSync(join(process.cwd(), "openapi.json"), "utf-8"));
+
 app.use(express.json());
 
 app.get("/health", (_req, res) => {
   res.json({ status: "ok" });
+});
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get("/openapi.json", (_req, res) => {
+  res.json(swaggerDocument);
 });
 
 app.use("/api/categories", categoryRoutes);
